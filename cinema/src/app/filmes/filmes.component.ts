656dcbf485807dd86ff7e4af97c033d5b68c82e0
Frederico Gondin <108304623+fgondin@users.noreply.github.com>
2023-08-14 15:52:06 -0300

    mudanças (3/2)

--- cinema/src/app/filmes/filmes.component.ts
+++ cinema/src/app/filmes/filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Filme } from './modelo/filme.model'
+import { Filme } from './models/filme.model'
 
 @Component({
   selector: 'cin-filmes',
@@ -9,14 +9,14 @@ import { Filme } from './modelo/filme.model'
 export class FilmesComponent {
 
   filme: Filme[] = [
-    {id: "1", nome: "007 Goldeneye", src: "/assets/007-goldeneye.jpg", genero: "1"},
-    {id: "2", nome: "Duro de Matar: A Vingança", src: "/assets/duro-de-matar.jpg", genero: "1"},
-    {id: "3", nome: "O Exterminador do Futuro", src: "/assets/o-exterminador.jpg", genero: "1"},
-    {id: "4", nome: "A Bruxa de Blair", src: "/assets/a-bruxa", genero: "2"},
-    {id: "5", nome: "O Chamado", src: "/assets/o-chamado", genero: "2"},
-    {id: "6", nome: "O Silêncio dos Inocentes", src: "/assets/o-silencio.jpg", genero: "2"},
-    {id: "7", nome: "Uma Linda Mulher", src: "/assets/uma-linda.jpg", genero: "3"},
-    {id: "8", nome: "Encontro Marcado", src: "/assets/encontro-marcado.jpg", genero: "3"},
-    {id: "9", nome: "Amor Além Da Vida", src: "/assets/amor-alem.jpg", genero: "3"},
+    {id: "1", nome: "007 Goldeneye", src: "/assets/img/007-goldeneye.jpg", genero: "1"},
+    {id: "2", nome: "Duro de Matar: A Vingança", src: "/assets/img/duro-de-matar.jpg", genero: "1"},
+    {id: "3", nome: "O Exterminador do Futuro", src: "/assets/img/o-exterminador.jpg", genero: "1"},
+    {id: "4", nome: "A Bruxa de Blair", src: "/assets/img/a-bruxa.jpg", genero: "2"},
+    {id: "5", nome: "O Chamado", src: "/assets/img/o-chamado.jpg", genero: "2"},
+    {id: "6", nome: "O Silêncio dos Inocentes", src: "/assets/img/o-silencio.jpg", genero: "2"},
+    {id: "7", nome: "Uma Linda Mulher", src: "/assets/img/uma-linda.jpg", genero: "3"},
+    {id: "8", nome: "Encontro Marcado", src: "/assets/img/encontro-marcado.jpg", genero: "3"},
+    {id: "9", nome: "Amor Além Da Vida", src: "/assets/img/amor-alem.jpg", genero: "3"},
   ]
 }
